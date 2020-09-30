2af31fb2b4b35c63eded910a66a8f4a5f75b246f
Emilia Szymanska <esz.emilia@gmail.com>
2020-09-30 23:44:48 +0200

    AddNote function

---
+++ scripts/notes.js
@@ -0,0 +1,21 @@
+let noteNumber = 0;
+const addButton = document.querySelector('.add-note');
+
+function addNote(title, description) {
+    const addNewNote = function () {
+        noteNumber++;
+
+        const template = document.querySelector('#note-template');
+        const clonedTemplate = document.importNode(template.content, true);
+
+        clonedTemplate.querySelector('input.note-title').value = title;
+        clonedTemplate.querySelector('textarea.note-text').value = description;
+
+        return clonedTemplate;
+    }
+
+    const note = addNewNote();
+    document.getElementById('#notes').appendChild(note);
+}
+
+addButton.addEventListener('click', () => addNote())
