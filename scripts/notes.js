54afec97f0a8b6750c8fe381135f8951b05ef245
Emilia Szymanska <esz.emilia@gmail.com>
2020-10-02 23:10:56 +0200

    Add saveData function

--- scripts/notes.js
+++ scripts/notes.js
@@ -1,21 +1,37 @@
 let noteNumber = 0;
 const addButton = document.querySelector('.add-note');
 
-function addNote(title = '', description = '') {
+(() => {
+    addButton.addEventListener('click', () => addNote());
+}) ();
+
+function addNote(title = '', content = '') {
     const addNewNote = function () {
         noteNumber++;
 
         const template = document.querySelector('#note-template');
         const clonedTemplate = document.importNode(template.content, true);
 
+        clonedTemplate.querySelector('div.note').setAttribute('id', 'id_' + noteNumber);
         clonedTemplate.querySelector('input.note-title').value = title;
-        clonedTemplate.querySelector('textarea.note-text').value = description;
+        clonedTemplate.querySelector('textarea.note-text').value = content;
 
         return clonedTemplate;
     }
 
     const note = addNewNote();
-    document.getElementById('notes').appendChild(note);
+    document.querySelector('#notes').appendChild(note);
 }
 
-addButton.addEventListener('click', () => addNote())
+function saveData() {
+    let notes = document.querySelectorAll('div.note');
+
+    for (let i = 0; i < notes.length; i++) {
+        let note = notes[i];
+
+        let noteJSON = {'title': note.querySelector('input.note-title').value,
+                        'content': note.querySelector('textarea.note-text').value};
+
+        localStorage.setItem(note.id, JSON.stringify(noteJSON));
+    }
+}
